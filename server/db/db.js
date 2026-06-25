c51ecd01182666cf9edc67507fd3e5f1e8f47f45
DavidOde100 <david.odejimi677@gmail.com>
2026-06-24 19:44:20 -0500

    Implement dotenv configuration and restructure database connection; add gift data and reset script

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,15 @@
+import dotenv from 'dotenv'
 import pg from 'pg'
 const { Pool } = pg;
 
-const connectionString =
-  "postgresql://postgres:t5u3i8hAKi0xFA1BP7Ri@containers-us-west-194.railway.app:6020/railway";
+dotenv.config({ path: '../.env' })
 
 export const pool = new Pool({
-  connectionString,
-});
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT) || 5432,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+})
 
 export default pool;
